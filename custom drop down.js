289e014906f319e6a83e9610a4434205fa0b2b97
zencap5 <55677996+zencap5@users.noreply.github.com>
2022-09-03 22:28:56 -0400

    Update custom drop down.js

--- custom drop down.js
+++ custom drop down.js
@@ -13,6 +13,50 @@ function app() {
         {
         label: "Artificial Reality / Virtual Reality",
         value: "Artificial Reality / Virtual Reality"
+        },
+        {
+        label: "Big Data / Analytics / Business Intelligence",
+        value: "Big Data / Analytics / Business Intelligence"
+        },
+        {
+        label: "Data Science",
+        value: "Data Science"
+        },
+        {
+        label: "Mobile Applications",
+        value: "Mobile Applications"
+        },
+        {
+        label: "Internet of Things (IoT)",
+        value: "Internet of Things (IoT)"
+        },
+        {
+        label: "Natural Language Processing",
+        value: "Natural Language Processing"
+        },
+        {
+        label: "Industrial Robotics / Automation",
+        value: "Industrial Robotics / Automation"
+        },
+        {
+        label: "Cyber Security",
+        value: "Cyber Security"
+        },
+        {
+        label: "SaaS-Cloud Computing",
+        value: "SaaS-Cloud Computing"
+        },
+        {
+        label: "Cloud Infrastructure (laas / PaaS)",
+        value: "Cloud Infrastructure (laas / PaaS)"
+        },
+        {
+        label: "Networking",
+        value: "Networking"
+        },
+        {
+        label: "Programming / Development",
+        value: "Programming / Development"
         }
     ];
 
